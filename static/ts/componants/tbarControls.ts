77a3a84d8e2522ef4c641fa1deaed7d26f45c0c0
Josh Pond <jtpond42@gmail.com>
2020-09-19 20:55:06 -0400

    Better responsive top bar

--- static/ts/componants/tbarControls.ts
+++ static/ts/componants/tbarControls.ts
@@ -20,10 +20,11 @@ export class TBarControls {
     let vh = 2.75*window.innerHeight/100.0;
     let counter = 1;
     let curr = this.current;
-    while (vw < (curr.length * vh)) {
-      let cells = curr.split('/');
-      cells[counter] = '.';
-      curr = cells.join('/');
+    let curr_cells = curr.split('/');
+    while (vw < (curr.length * vh) && counter < curr_cells.length-1 ){
+      curr_cells[counter] = (counter == curr_cells.length-2)? '.':'..';
+      curr = curr_cells.join('/');
+      curr_cells = curr.split('/');
       counter += 1;
     }
     let out = document.createElement('div');
@@ -35,9 +36,10 @@ export class TBarControls {
     out.append(root_link);
     let th_link = '/';
     let cells = curr.split('/');
-    let fold_links = cells.slice(1,cells.length-1).map((chunk) => {
+    let fCells = this.current.split('/').slice(1,cells.length-1);
+    let fold_links = cells.slice(1,cells.length-1).map((chunk,i) => {
       if (chunk !== '') {
-        th_link = th_link.concat(`${chunk}/`);
+        th_link = th_link.concat(`${fCells[i]}/`);
         let fold_link = document.createElement('span');
         fold_link.classList.add('folder__link');
         fold_link.textContent = `${chunk}/`;
