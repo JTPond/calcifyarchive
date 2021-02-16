53519f61ccc3d5640b08b6b0194be9e7b08c9524
Duy Nguyen <dtn5ah@virginia.edu>
2021-02-15 22:36:51 -0500

    Add Powershell Build Script

---
+++ static/build.ps1
@@ -0,0 +1,6 @@
+Write-Host "Creating dist"
+mkdir ..\dist\style\css\,..\dist\js\
+Write-Host "Copying files"
+cp .\index.html ..\dist\index.html
+type .\style\css\theme.css,.\style\css\stylesheet.css > ..\dist\style\css\stylesheet.css
+cp .\style\assets\* ..\dist\style\
