675b23a53df65a7153a948289d77d46e2baba484
Duy Nguyen <dtn5ah@virginia.edu>
2021-06-29 22:40:47 -0400

    Un-Alias Powershell cmdlets

--- static/build.ps1
+++ static/build.ps1
@@ -1,7 +1,7 @@
 Write-Host "Creating dist"
-mkdir ..\dist\style\css\,..\dist\js\
+mkdir ..\dist\style\css\, ..\dist\js\
 Write-Host "Copying files"
-cp .\index.html ..\dist\index.html
-type .\style\css\theme.css,.\style\css\stylesheet.css > ..\dist\style\css\stylesheet.css
-cp .\style\assets\* ..\dist\style\
+Copy-Item .\index.html ..\dist\index.html
+Get-Content .\style\css\theme.css, .\style\css\stylesheet.css > ..\dist\style\css\stylesheet.css
+Copy-Item .\style\assets\* ..\dist\style\
 tsc
