393586260c9a698bdfbdec07740ffa6b8ac82a77
Duy Nguyen <dtn5ah@virginia.edu>
2021-06-30 23:30:04 -0400

    DOS Build: Preserve assets folder upon copy

--- static/build.bat
+++ static/build.bat
@@ -1,8 +1,8 @@
 echo Creating dist
-mkdir ..\dist\style\css\ ..\dist\js\
+mkdir ..\dist\style\css\ ..\dist\style\assets\ ..\dist\js\
 echo Copying files
 copy /y .\index.html ..\dist\index.html
 copy /y .\style\css\theme.css+.\style\css\stylesheet.css  ..\dist\style\css\stylesheet.css
-copy /y .\style\assets\* ..\dist\style\.
+copy /y .\style\assets\* ..\dist\style\assets\.
 echo Building Typescript
 tsc
