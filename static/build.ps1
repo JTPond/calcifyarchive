42c7c16bc46ccd8d07074392837dbc01cb8066f6
Duy Nguyen <dtn5ah@virginia.edu>
2021-06-30 23:17:26 -0400

    PS Build: Copy assets recursively to get folder structure

--- static/build.ps1
+++ static/build.ps1
@@ -3,5 +3,5 @@ mkdir ..\dist\style\css\, ..\dist\js\
 Write-Host "Copying files"
 Copy-Item .\index.html ..\dist\index.html
 Get-Content .\style\css\theme.css, .\style\css\stylesheet.css > ..\dist\style\css\stylesheet.css
-Copy-Item .\style\assets\* ..\dist\style\
+Copy-Item -R .\style\assets ..\dist\style\
 tsc
